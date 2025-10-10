fffea0253df64eb4f74480fa18c4329a8a57c4ff
Andrew Yurovchak <andy@yurovchak.net>
2025-10-10 18:18:47 -0400

    fix for dynamic import error

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-import commonjs from '@rollup/plugin-commonjs';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit(), tailwindcss()],
-	build: {
-		"rollupOptions": {
-			plugins: [commonjs({
-				dynamicRequireTargets: [
-					'node_modules/@libsql/linux-x64-musl/index.node'
-				],
-				ignoreDynamicRequires: false
-			})]
-		}
-	}
 });
