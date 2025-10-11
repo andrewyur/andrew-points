b1d6f1ba1fa47868b51d6ac30068802f1e3f2075
Andrew Yurovchak <andy@yurovchak.net>
2025-10-10 23:42:44 -0400

    compose + fix auth

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -1,4 +1,4 @@
 import { Discord } from "arctic";
-import { DISCORD_CLIENT_SECRET, DISCORD_CLIENT_ID } from "$env/static/private";
+import { DISCORD_CLIENT_SECRET, DISCORD_CLIENT_ID, APP_URL } from "$env/static/private";
 
-export const discord = new Discord(DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, "http://testdomain.nut:5173/login/discord/callback")
+export const discord = new Discord(DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, `${APP_URL}/login/discord/callback`)
